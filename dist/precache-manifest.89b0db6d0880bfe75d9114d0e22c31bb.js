b2fdd7e405276b9325da1a6a21f254116b7766ff
JCrosby0 <jcrosby@antaeus-tech.com>
2021-02-18 16:10:14 +0800

    pushing dist to see if it can run on github pages

---
+++ dist/precache-manifest.89b0db6d0880bfe75d9114d0e22c31bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ce5ec6918314b77b87",
+    "url": "/css/app.297bae01.css"
+  },
+  {
+    "revision": "720de524423f38a0b992",
+    "url": "/css/chunk-vendors.d4ec59ac.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3105c532ed31ef21789b30ca828fb8d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53ce5ec6918314b77b87",
+    "url": "/js/app.0d19833c.js"
+  },
+  {
+    "revision": "720de524423f38a0b992",
+    "url": "/js/chunk-vendors.2e02d8f9.js"
+  },
+  {
+    "revision": "4a9ea2788d8893e746c29a3af0df86d7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
